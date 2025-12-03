efc1c20e2dbf787359ab186ccaabdec14362827e
Danny Behar <dannybehar@icloud.com>
2025-12-03 11:01:26 -0500

    initial commit

--- Sources/PDFViewer/PDFViewer.swift
+++ Sources/PDFViewer/PDFViewer.swift
@@ -1,2 +1,18 @@
-// The Swift Programming Language
-// https://docs.swift.org/swift-book
+import SwiftUI
+import PDFKit
+
+public struct PDFViewer: UIViewRepresentable {
+    let document: PDFDocument
+    
+    public init(document: PDFDocument) {
+        self.document = document
+    }
+    
+    public func makeUIView(context: Context) -> PDFView {
+       return PDFView()
+    }
+    
+    public func updateUIView(_ view: PDFView, context: Context) {
+       
+    }
+}
