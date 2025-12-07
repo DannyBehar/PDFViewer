ba51b88ef47b8123920c6ffbf07fcc9dcc4bae5a
Danny Behar <dannybehar@icloud.com>
2025-12-06 20:32:54 -0500

    update minimum version to iOS 17

--- Package.swift
+++ Package.swift
@@ -5,7 +5,7 @@ import PackageDescription
 
 let package = Package(
     name: "PDFViewer",
-    platforms: [.iOS("16.4"), .macOS(.v11), .visionOS(.v1)],
+    platforms: [.iOS("17.0"), .macOS(.v11), .visionOS(.v1)],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
